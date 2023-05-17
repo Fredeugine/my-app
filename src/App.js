ce0016dfe311660eaa758ff4de229c05995ed427
Fred Eugine Adomako <vimportantemails@gmail.com>
2023-05-17 21:14:03 +0200

    React exercise 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function hello() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>HELLO</h1>
+        <p>It's time to learn React!!!</p>
+      </div>
   );
 }
 
-export default App;
+export default hello;
+
